5371e56fc09370c6ddd74416a7df11824ec0ca00
MatheusHVBotelho <166166381+MatheusHVBotelho@users.noreply.github.com>
2025-10-22 12:27:40 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ThemeProvider } from "./ThemeContext"; // 👈 Importa o contexto
+import { ThemeProvider } from "./ThemeContext";
 
 // Telas
 import HomeScreen from "./InicioScreen";
