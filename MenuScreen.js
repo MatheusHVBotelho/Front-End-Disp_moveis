d353809b788e92da72b60a415a285d93a42fb2ad
MatheusHVBotelho <166166381+MatheusHVBotelho@users.noreply.github.com>
2025-10-22 12:30:38 -0300

    Update MenuScreen.js

--- MenuScreen.js
+++ MenuScreen.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { SafeAreaView, View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { useTheme } from "./ThemeContext"; // 👈 Importa o contexto do tema
+import { useTheme } from "./ThemeContext"; 
 
 export default function MenuScreen({ navigation }) {
-  const { tema } = useTheme(); // 👈 Usa o tema atual
+  const { tema } = useTheme(); 
 
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: tema.background }]}>
