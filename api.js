164a55cf6c8c5a13f83c5f70df9c5226110dc1c5
MatheusHVBotelho <166166381+MatheusHVBotelho@users.noreply.github.com>
2025-10-22 12:33:18 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,5 @@
-// src/api.js
 import axios from "axios";
 
-// Base URL da sua API (ajuste conforme o endereço real)
 const api = axios.create({
   baseURL: "https://api-questoes-production.up.railway.app",
 });
